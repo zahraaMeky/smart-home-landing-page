3eb7a62245f0098573d1667f5522890dc10ca2fe
zahraa Meky <eng.alzahraa.meky@gmail.com>
2024-05-07 21:23:03 +0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        '16': '16rem',
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
